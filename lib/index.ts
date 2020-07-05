20da7a209d6d825cff54adac4d5e69b323670965
mardets <67030144+mardets@users.noreply.github.com>
2020-07-05 17:36:07 +0200

    Delete index.ts

--- lib/index.ts
+++
@@ -1,9 +0,0 @@
-import {ApplicationModule} from './application/module.js';
-import {ApplicationCore} from './core/core.js';
-import {ApplicationProvider} from'./provider/provider.js';
-
-export declare namespace Waza {
-	//var application : ApplicationModule;
-	//var core : ApplicationCore;
-	//var provider : ApplicationProvider;
-}
